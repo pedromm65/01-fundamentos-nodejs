2c67108b38d482397a42f51c93daed2345bacded
pedrohlimadev <pedrohlimadev@gmail.com>
2023-01-31 11:51:51 -0300

    Finalizando sub modulo Estrutura da aplicação

--- src/server.js
+++ src/server.js
@@ -1,19 +1,27 @@
 import http from 'node:http'
 
+const users = []
 
 const server = http.createServer((req, res) => {
 
   const { method, url } = req
 
   if(method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users') {
-    return res.end('Criação de usuarios')
+    users.push({
+      id: 1,
+      name: 'Pedro',
+      email: 'pedro@email.com'
+    })
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello Ignite')
+  return res.writeHead(404).end()
 
 
 })
